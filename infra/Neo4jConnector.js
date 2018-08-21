1431201a81670786cfe2bfd62418c8fa1bdb574e
Yan Mendes <ymendesf@gmail.com>
2018-08-21 11:57:26 -0300

    minor corrections

--- infra/Neo4jConnector.js
+++ infra/Neo4jConnector.js
@@ -2,4 +2,4 @@ const neo4j = require('neo4j-driver').v1;
 
 const driver = neo4j.driver(process.env.NEO4JURI, neo4j.auth.basic(process.env.NEO4JUSER, process.env.NEO4JPASSWORD));
 
-module.exports = driver;
+module.exports = driver.session();
