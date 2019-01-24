62bff04df9fd876543033d6598d3a24c22111758
Yan Ferreira <yan.ferreira@escale.com.br>
2019-01-24 15:58:55 -0200

    Wrap the request around a try, catch block

--- routes/query.js
+++ routes/query.js
@@ -4,21 +4,27 @@ const
   { resolveContext, contexts } = require('../query-parsers');
 
 router.post('/', async (req, res, next) => {
-  let { context, query } = resolveContext(req.body.query)
+  try {
+    let { context, query } = resolveContext(req.body.query)
 
-  //Nested queries in bigdawg
-  if (typeof query === 'object')
-    throw new error("Can't handle that yet")
+    //Nested queries in bigdawg
+    if (typeof query === 'object')
+      throw new error("Can't handle that yet")
 
-  let { parser, dbInterface } = contexts[context]
+    let { parser, dbInterface } = contexts[context]
 
-  query = await parser(query)
-  dbInterface(query, results => {
-    res.send({
-      success: true,
-      results: results
+    query = await parser(query)
+    results = await dbInterface(query, results => {
+      console.log(query)
+      res.send({
+        success: true,
+        results: results
+      })
     })
-  })
+
+  } catch(e) {
+    next(req, res, next, e)
+  }
 });
 
 module.exports = router;
