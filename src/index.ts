c2f0b0dc8bad5dd779fbd2954297b22d81dac6dc
Yan Ferreira <yan.ferreira@escale.com.br>
2019-02-23 11:50:26 -0300

    Refactor allowing CORS on graphql using env variables

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,23 @@
 import 'reflect-metadata'
+import 'dotenv/config'
 import { ApolloServer } from 'apollo-server-express'
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
-import * as cors from 'cors'
 import * as session from 'express-session'
-import * as path from 'path'
-import * as dotenv from 'dotenv'
 import * as pino from 'express-pino-logger'
 import { createConnection } from 'typeorm'
 
 import { port } from './config'
 import logger from './logger'
 import query from './routes/query'
-import user from './routes/user'
 import typeDefs from './types/GraphQLTypes'
 import resolvers from './resolvers'
-
-if (process.env.NODE_ENV === 'production') {
-  dotenv.config({ path: path.join(__dirname, '/../envs/.prod') })
-} else {
-  dotenv.config({ path: path.join(__dirname, '../../envs/.dev') })
-}
 
 const startServer = async () => {
 
   await createConnection()
 
   const app = express()
-  app.use(cors())
 
   app.use(pino({ logger }))
 
@@ -37,7 +27,6 @@ const startServer = async () => {
   app.get('/', (_, res) => res.status(200).send('ok!'))
 
   app.use('/query', query)
-  app.use('/user', user)
 
   const server = new ApolloServer({
     typeDefs,
@@ -57,7 +46,7 @@ const startServer = async () => {
     app,
     cors: {
       credentials: true,
-      origin: '*'
+      origin: process.env.FRONTEND_URL
     }
   })
 
