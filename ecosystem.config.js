eee0291a3eebe9d0af92b1d2f176978635fb581b
Yan Ferreira <yan.ferreira@escale.com.br>
2019-04-06 10:23:11 -0300

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'polyflow-api',
-      script: './dist/src',
+      name: 'polyflow.api',
+      script: 'yarn',
+      args: 'run start:prod',
       watch: true,
       env_production: {
         NODE_ENV: 'production'
