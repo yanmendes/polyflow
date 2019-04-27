18e466b1f170c162f4470075c9eef791d3bbb90e
Yan Ferreira <yan.ferreira@escale.com.br>
2019-04-26 21:40:58 -0300

    Extract dataSource module

---
+++ src/GraphQL/resolvers/Mutation/dataSource.ts
@@ -0,0 +1,43 @@
+import { getConnection } from "typeorm";
+import { UserInputError } from "apollo-server-core";
+
+import { getInterface } from "../../../databases";
+import { getWorkspace } from "../../../services";
+import { DataSource } from "../../../models/polyflow";
+import logger, { categories } from "../../../logger";
+
+const log = logger.child({
+  category: categories.DATA_SOURCE
+});
+
+export default {
+  addDataSource: async (_, { workspaceId, type, uri }, { req }) => {
+    const workspace = await getWorkspace(req, workspaceId);
+
+    const dbInterface = getInterface(type);
+    const validConnection = await dbInterface.assertConnection(uri);
+
+    if (!validConnection) {
+      throw new UserInputError("Invalid URI for the chosen type");
+    }
+
+    try {
+      const dataSource = await DataSource.create({ uri, type }).save();
+      await getConnection()
+        .createQueryBuilder()
+        .relation(DataSource, "workspace")
+        .of(dataSource)
+        .set(workspace);
+
+      return true;
+    } catch (e) {
+      log
+        .child({
+          error: e,
+          action: "adding_data_source"
+        })
+        .error("URI already registered in this workspace");
+      throw new UserInputError("URI already registered in this workspace");
+    }
+  }
+};
--- src/GraphQL/resolvers/Mutation/index.ts
+++ src/GraphQL/resolvers/Mutation/index.ts
@@ -1,47 +1,11 @@
-import { getConnection } from "typeorm";
-import { UserInputError } from "apollo-server-core";
-
-import { getInterface } from "../../../databases";
 import userMutations from "./user";
 import workspaceMutations from "./workspace";
-import { runQuery, getWorkspace } from "../../../services";
-import { DataSource } from "../../../models/polyflow";
-import logger, { categories } from "../../../logger";
+import dataSourceMutations from "./dataSource";
+import { runQuery } from "../../../services";
 
 export default {
   ...userMutations,
   ...workspaceMutations,
-
-  addDataSource: async (_, { workspaceId, type, uri }, { req }) => {
-    const workspace = await getWorkspace(req, workspaceId);
-
-    const dbInterface = getInterface(type);
-    const validConnection = await dbInterface.assertConnection(uri);
-
-    if (!validConnection) {
-      throw new UserInputError("Invalid URI for the chosen type");
-    }
-
-    try {
-      const dataSource = await DataSource.create({ uri, type }).save();
-      await getConnection()
-        .createQueryBuilder()
-        .relation(DataSource, "workspace")
-        .of(dataSource)
-        .set(workspace);
-
-      return true;
-    } catch (e) {
-      logger
-        .child({
-          error: e,
-          category: categories.DATA_SOURCE,
-          action: "adding_data_source"
-        })
-        .error("URI already registered in this workspace");
-      throw new UserInputError("URI already registered in this workspace");
-    }
-  },
-
+  ...dataSourceMutations,
   query: (_, { query }, { req }) => runQuery(query, req)
 };
