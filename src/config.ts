ea972a1ef99f61b4ac23ac9f9e2127f8673e41b7
Yan Ferreira <yan.ferreira@escale.com.br>
2019-05-17 11:10:12 -0300

    Change default port in dev mode

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const port: number = parseInt(process.env.PORT || "3000", 10);
+export const port: number = parseInt(process.env.PORT || "3050", 10);
 export const psqlURL: string = process.env.TYPEORM_URL || "postgres://postgres:postgres@localhost/polyflow";
