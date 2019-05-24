f27f7480cb716bee1135a4e42a39b4276e26e212
Yan Ferreira <yan.ferreira@escale.com.br>
2019-05-24 16:32:51 -0300

    Implement Swift mediator

---
+++ examples/Provenance/Swift.js
@@ -0,0 +1,115 @@
+const Prov = require('./Prov')
+const Provone = require('./Provone')
+
+const provoneProgram = {
+  name: 'script_run',
+  columns: [
+    {
+      alias: 'program_id',
+      projection: 'script_run_id'
+    },
+    {
+      alias: 'label',
+      projection: 'script_filename'
+    }
+  ]
+}
+
+const provoneExecution = {
+  name: 'app_exec',
+  columns: [
+    {
+      alias: 'execution_id',
+      projection: 'app_exec_id'
+    },
+    {
+      alias: 'start_time',
+      projection: 'prov_startedAtTime'
+    },
+    {
+      alias: 'script_run_id',
+      projection: 'provone_hadPlan'
+    },
+    {
+      projection: 'duration'
+    }
+  ]
+}
+
+const provEntity = {
+  name: 'file',
+  columns: [
+    {
+      alias: 'entity_id',
+      projection: 'file_id'
+    },
+    {
+      alias: 'label',
+      projection: 'name'
+    },
+    {
+      alias: 'entity_type',
+      projection: "'provone_Document'"
+    }
+  ]
+}
+
+const provUsed = {
+  name: 'staged_in',
+  columns: [
+    {
+      alias: 'entity_id',
+      projection: 'file_id'
+    },
+    {
+      alias: 'execution_id',
+      projection: 'app_exec_id'
+    }
+  ]
+}
+
+const provWasGeneratedBy = {
+  name: 'staged_out',
+  columns: [
+    {
+      alias: 'entity_id',
+      projection: 'file_id'
+    },
+    {
+      alias: 'execution_id',
+      projection: 'app_exec_id'
+    }
+  ]
+}
+
+const entityMappers = [
+  {
+    name: Prov.Classes.ENTITY,
+    slug: Prov.Classes.ENTITY,
+    entityMapper: provEntity
+  },
+  {
+    name: Provone.Classes.PROGRAM,
+    slug: Provone.Classes.PROGRAM,
+    entityMapper: provoneProgram
+  },
+  {
+    name: Provone.Classes.EXECUTION,
+    slug: Provone.Classes.EXECUTION,
+    entityMapper: provoneExecution
+  },
+  {
+    name: Prov.Relationships.USED,
+    slug: Prov.Relationships.USED,
+    entityMapper: provUsed
+  },
+  {
+    name: Prov.Relationships.WASGENERATEDBY,
+    slug: Prov.Relationships.WASGENERATEDBY,
+    entityMapper: provWasGeneratedBy
+  }
+]
+
+console.log(JSON.stringify(entityMappers, null, 2))
+
+module.exports = entityMappers
