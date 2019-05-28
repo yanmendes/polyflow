d1a16d5339803fe6080affa9176c36b42edfdf77
Yan Ferreira <yan.ferreira@escale.com.br>
2019-05-28 20:22:28 -0300

    Remove bad error treatment It was suppressing different errors

--- src/GraphQL/resolvers/Mutation/entity.ts
+++ src/GraphQL/resolvers/Mutation/entity.ts
@@ -1,13 +1,7 @@
 import { getRepository } from "typeorm";
-import { UserInputError } from "apollo-server-core";
 
 import { getMediator } from "../../../services";
 import { Entity } from "../../../models/polyflow";
-import logger, { categories } from "../../../logger";
-
-const log = logger.child({
-  category: categories.ENTITY
-});
 
 export default {
   addEntity: async (
@@ -15,24 +9,12 @@ export default {
     { name, slug, entityMapper, workspaceId, dataSourceId, mediatorId },
     { req }
   ) =>
-    getMediator(req, workspaceId, dataSourceId, mediatorId)
-      .then(mediator =>
-        getRepository(Entity).save({
-          name,
-          slug,
-          entityMapper,
-          mediator
-        })
-      )
-      .catch(e => {
-        log
-          .child({
-            error: e,
-            action: "adding_mediator"
-          })
-          .error("Entity name/slug already in use for this mediator");
-        throw new UserInputError(
-          "Entity name/slug already in use for this mediator"
-        );
+    getMediator(req, workspaceId, dataSourceId, mediatorId).then(mediator =>
+      getRepository(Entity).save({
+        name,
+        slug,
+        entityMapper,
+        mediator
       })
+    )
 };
