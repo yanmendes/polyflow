8f832a20edf3dac6283f858d531a59c2ff5fecca
Yan Mendes <ymendesf@gmail.com>
2019-11-03 18:34:48 -0300

    fix: setup new aws' secrets

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 export const port: number = parseInt(process.env.PORT || '3050', 10)
-const awsSecrets: any = process.env.AWS_SECRETS
-console.log(awsSecrets, awsSecrets.TYPEORM_URL)
+const awsSecrets: any = JSON.parse(process.env.AWS_SECRETS)
 export const psqlURL: string =
-  (JSON.parse(awsSecrets) || {}).TYPEORM_URL ||
+  (awsSecrets || {}).TYPEORM_URL ||
   process.env.TYPEORM_URL ||
   'postgres://postgres:postgres@localhost/polyflow'
 export const JWT_SECRET: string = process.env.JWT_SECRET || 'gwrag3qgragrgqe'
