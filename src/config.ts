db710fbaa44a2c725b041199d0964b9cdb318f35
Yan Mendes <ymendesf@gmail.com>
2019-11-03 13:15:58 -0300

    feat: import secrets from aws' SSM

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 export const port: number = parseInt(process.env.PORT || '3050', 10)
+const awsSecrets: any = process.env.AWS_SECRETS
 export const psqlURL: string =
-  process.env.TYPEORM_URL || 'postgres://postgres:postgres@localhost/polyflow'
+  (awsSecrets || {}).TYPEORM_URL ||
+  process.env.TYPEORM_URL ||
+  'postgres://postgres:postgres@localhost/polyflow'
 export const JWT_SECRET: string = process.env.JWT_SECRET || 'gwrag3qgragrgqe'
