98365fb896088c75d19ca7de4a2ba21141dab95b
Yan Mendes <ymendesf@gmail.com>
2019-11-03 18:34:48 -0300

    feat: debug

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const port: number = parseInt(process.env.PORT || '3050', 10)
 const awsSecrets: any = process.env.AWS_SECRETS
+console.log(awsSecrets, awsSecrets.TYPEORM_URL)
 export const psqlURL: string =
-  (awsSecrets || {}).TYPEORM_URL ||
+  (JSON.parse(awsSecrets) || {}).TYPEORM_URL ||
   process.env.TYPEORM_URL ||
   'postgres://postgres:postgres@localhost/polyflow'
 export const JWT_SECRET: string = process.env.JWT_SECRET || 'gwrag3qgragrgqe'
