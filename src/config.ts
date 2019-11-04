f32151370f4023ca01f2806b6c8d47e81ff700eb
Yan Mendes <ymendesf@gmail.com>
2019-11-04 00:31:57 -0300

    fix: setup default value to env var when parsing

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const port: number = parseInt(process.env.PORT || '3050', 10)
-const awsSecrets: any = JSON.parse(process.env.AWS_SECRETS)
+const awsSecrets: any = JSON.parse(process.env.AWS_SECRETS || '{}')
 export const psqlURL: string =
-  (awsSecrets || {}).TYPEORM_URL ||
+  awsSecrets.TYPEORM_URL ||
   process.env.TYPEORM_URL ||
   'postgres://postgres:postgres@localhost/polyflow'
 export const JWT_SECRET: string = process.env.JWT_SECRET || 'gwrag3qgragrgqe'
