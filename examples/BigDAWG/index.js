ee028679ade59a736939a12bb7e955d299085a4b
Yan Mendes <ymendesf@gmail.com>
2020-04-17 15:02:47 -0300

    chore: refactor BigDAWG example and add dev wf

--- examples/BigDAWG/index.js
+++ examples/BigDAWG/index.js
@@ -9,6 +9,7 @@ const swiftEntities = require('./Swift')
 const polyflowUri = process.env.POLYFLOW_URI || 'http://localhost:3050/'
 const bigdawgUri =
   process.env.BIGDAWG_URL || 'http://localhost:8080/bigdawg/query'
+const dev = process.env.NODE_ENV !== 'production'
 
 const dataSource = {
   uri: bigdawgUri,
@@ -53,22 +54,22 @@ const addEntity = gql`
   }
 `
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: polyflowUri,
+    fetch
+  }),
+  cache: new InMemoryCache({})
+})
+
+const mutate = (mutation, variables) =>
+  client.mutate({
+    mutation,
+    variables
+  })
+
 ;(async () => {
   try {
-    const client = new ApolloClient({
-      link: new HttpLink({
-        uri: polyflowUri,
-        fetch
-      }),
-      cache: new InMemoryCache({})
-    })
-
-    const mutate = (mutation, variables) =>
-      client.mutate({
-        mutation,
-        variables
-      })
-
     await mutate(addDataSource, { dataSource })
     await Promise.all(
       mediators.map(mediator => mutate(addMediator, { mediator }))
@@ -85,7 +86,21 @@ const addEntity = gql`
     )
     console.log('Done!')
   } catch (e) {
-    console.error(e)
-    process.exit(2)
+    if (!dev) {
+      console.error(e)
+      return process.exit(2)
+    }
+    Promise.all([
+      ...keplerEntities.map(e =>
+        mutate(addEntity, { entity: { ...e, mediatorSlug: 'kepler' } }).catch(
+          e => console.log(`Ignoring error: ${e}`)
+        )
+      ),
+      ...swiftEntities.map(e =>
+        mutate(addEntity, { entity: { ...e, mediatorSlug: 'swift' } }).catch(
+          e => console.log(`Ignoring error: ${e}`)
+        )
+      )
+    ]).then(() => console.log('Done!'))
   }
 })()
